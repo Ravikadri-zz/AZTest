2d2980385f547519399760118f31f1cd4d3f5936
Ravi Chandra <ravi.kadri10@gmail.com>
2022-02-23 17:21:07 -0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
-m365 login --authtype identity --userName 584c8a35-be83-47f6-b0da-8ef5d51f8a97
+m365 login --authType identity --userName 584c8a35-be83-47f6-b0da-8ef5d51f8a97
 m365 spo site classic list --output json
