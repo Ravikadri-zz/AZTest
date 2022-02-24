02efe74bd598450a4ee5c1b544f5b3cc86c93d74
Ravi Chandra <ravi.kadri10@gmail.com>
2022-02-24 08:53:59 -0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
-m365 login --authType identity --userName 584c8a35-be83-47f6-b0da-8ef5d51f8a97
-m365 aad o365group recyclebinitem list --output json
+m365 login --authType identity --userName 584c8a35-be83-47f6-b0da-8ef5d51f8a97  --verbose
+m365 aad o365group recyclebinitem list --output json --verbose
 
